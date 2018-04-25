d767db8d60ee959e4c73b75bd16afc7596183277
mcllsti <darylmcallister@hotmail.co.uk>
2018-04-25 21:59:23 +0100

    Unit tests added for UserDAO

--- Testing/wpd2/teamR/dao/UserDAOTest.java
+++ Testing/wpd2/teamR/dao/UserDAOTest.java
@@ -1,8 +1,14 @@
 package wpd2.teamR.dao;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import wpd2.teamR.models.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
@@ -12,15 +18,40 @@ public class UserDAOTest {
     public void setUp() throws Exception {
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+    @AfterClass
+    public static void tearDown() throws Exception {
 
-    @Test
-    public void checkIsValidUser() {
+        ConnectionSupplier cs = new ConnectionSupplier();
+        Connection connection = cs.provide();
+
+        String query = "DELETE FROM users WHERE fname = ?";
+
+        PreparedStatement ps = connection.prepareStatement(query);
+
+            //PASS ID TO PREPARED STATEMENT
+            ps.setString(1, "Test");
+
+            ps.executeUpdate();
     }
 
     @Test
     public void registerUser() {
+        UserDAO dao = new UserDAO();
+        User user = new User("Test","Test","Test@Test.com","Test");
+        assertTrue(dao.registerUser(user));
     }
+
+    @Test
+    public void checkIsValidUser() throws SQLException {
+        UserDAO dao = new UserDAO();
+        assertFalse(dao.checkIsValidUser("Test@Test.com","Test").isEmpty());
+    }
+
+    @Test
+    public void checkIsNotValidUser() throws SQLException {
+        UserDAO dao = new UserDAO();
+        assertTrue(dao.checkIsValidUser("JohnnyTravers","WillyWonka").isEmpty());
+    }
+
+
 }
