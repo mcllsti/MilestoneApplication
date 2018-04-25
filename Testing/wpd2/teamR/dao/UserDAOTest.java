7f565eae43b4b2c2a146e579856b4b5609e52a6c
mcllsti <darylmcallister@hotmail.co.uk>
2018-04-25 21:47:16 +0100

    Merged and cleaned up existing ProjectDAO code

---
+++ Testing/wpd2/teamR/dao/UserDAOTest.java
@@ -0,0 +1,26 @@
+package wpd2.teamR.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDAOTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void checkIsValidUser() {
+    }
+
+    @Test
+    public void registerUser() {
+    }
+}
