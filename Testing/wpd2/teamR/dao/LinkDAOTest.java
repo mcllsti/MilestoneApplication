9f0cdb28365e28f6a2505782f79fa1c95164ce4d
Chris Connor <cjconnor24@connordesign.com>
2018-04-26 09:45:55 +0100

    Adding test file...it's a completely mess at the moment so ignore ⚠️

--- Testing/wpd2/teamR/dao/LinkDAOTest.java
+++ Testing/wpd2/teamR/dao/LinkDAOTest.java
@@ -1,5 +1,61 @@
+package wpd2.teamR.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import wpd2.teamR.models.Link;
+
 import static org.junit.Assert.*;
 
 public class LinkDAOTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findById() {
+
+//        LinkDAO testingDAO = new LinkDAO();
+//        Link link = testingDAO.findById(5);
+//        assertEquals(link, new Link("chris@hotmail.com",6));
+
+    }
+
+    @Test
+    public void findByProjectId() {
+    }
+
+    @Test
+    public void findByUserId() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void save() {
+
+        LinkDAO testingDAO = new LinkDAO();
+        Link link = new Link("chris@hotmail.com");
+
+        assertTrue(testingDAO.save(link,6));
+    }
+
+    @Test
+    public void delete() {
+
+//        LinkDAO testingDAO = new LinkDAO();
+//        Link link = new Link("chris@hotmail.com",5);
+//        assertTrue(testingDAO.delete(link));
+    }
+
+    @Test
+    public void deleteByEmail() {
+    }
 }
