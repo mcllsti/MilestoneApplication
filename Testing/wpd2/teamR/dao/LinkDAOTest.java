996bcc5b526c0d68c71fb666d619ef6c375cbbce
mcllsti <darylmcallister@hotmail.co.uk>
2018-04-26 11:49:22 +0100

    Delete servlet working for Projects

--- Testing/wpd2/teamR/dao/LinkDAOTest.java
+++ Testing/wpd2/teamR/dao/LinkDAOTest.java
@@ -1,5 +1,46 @@
+package wpd2.teamR.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LinkDAOTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findByProjectId() {
+    }
+
+    @Test
+    public void findByUserId() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void deleteByEmail() {
+    }
 }
--- src/main/java/wpd2/teamR/Runner.java
+++ src/main/java/wpd2/teamR/Runner.java
@@ -51,7 +51,7 @@ public class Runner {
 
         handler.addServlet(new ServletHolder(new ProjectListServlet()), "/projects");
         handler.addServlet(new ServletHolder(new ProjectCreateServlet()), "/projects/create");
-
+        handler.addServlet(new ServletHolder(new ProjectDeleteServlet()), "/projects/delete/*");
         handler.addServlet(new ServletHolder(new PrivatePageServlet()), "/private");
         handler.addServlet(new ServletHolder(new LoginServlet()), "/login");
         handler.addServlet(new ServletHolder(new LogoutServlet()), "/logout");
