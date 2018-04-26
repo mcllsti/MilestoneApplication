5156328452648c59da8973a1cd3f2ff1d1973573
Gavin <gamac97@hotmail.co.uk>
2018-04-26 22:39:17 +0100

    Milestone model edits

--- src/main/java/wpd2/teamR/models/Milestone.java
+++ src/main/java/wpd2/teamR/models/Milestone.java
@@ -17,8 +17,7 @@ public class Milestone {
 
     private int projectID; // AGAIN, I DONT THINK WE NEED THIS TO BE HONEST ?
 
-
-    public Milestone(int id, String name, String description, Timestamp dateCreated, Timestamp dateModified, Timestamp dueDate, Timestamp dateCompleted) {
+    public Milestone(int id, String name, String description, Timestamp dateCreated, Timestamp dateModified, Timestamp dueDate, Timestamp dateCompleted, int projectID) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -26,64 +25,10 @@ public class Milestone {
         this.dateModified = dateModified;
         this.dueDate = dueDate;
         this.dateCompleted = dateCompleted;
+        this.projectID = projectID;
         
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Timestamp getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(Timestamp dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public Timestamp getDateModified() {
-        return dateModified;
-    }
-
-    public void setDateModified(Timestamp dateModified) {
-        this.dateModified = dateModified;
-    }
-
-    public Timestamp getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Timestamp dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Timestamp getDateCompleted() {
-        return dateCompleted;
-    }
-
-    public void setDateCompleted(Timestamp dateCompleted) {
-        this.dateCompleted = dateCompleted;
-    }
 
 
 }
